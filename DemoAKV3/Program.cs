7dbd0c636d3d9bf054fe4ba2fee6c04fe36fff31
De Lène Mirouze <fmirouze@hotmail.com>
2018-09-25 22:15:39 +0200

    authent certificat

--- DemoAKV3/Program.cs
+++ DemoAKV3/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using DemoAKV3.Utils;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
@@ -17,8 +19,12 @@ namespace DemoAKV3
         {
             string baseVaultAddress = "https://amethystevault.vault.azure.net";
 
+            //KeyVaultClient keyVaultClient = new KeyVaultClient(
+            //        new KeyVaultClient.AuthenticationCallback(GetAccessToken));
+
+            CertificateAccesTokenService cts = new CertificateAccesTokenService();
             KeyVaultClient keyVaultClient = new KeyVaultClient(
-                    new KeyVaultClient.AuthenticationCallback(GetAccessToken));
+                    new KeyVaultClient.AuthenticationCallback(cts.GetAccessToken));
 
             Console.WriteLine("Liste des secrets du vault");
             IPage<SecretItem> items = keyVaultClient
@@ -62,5 +68,78 @@ namespace DemoAKV3
        
             return authResult.AccessToken;
         }
+
+        //private static async Task<string> GetAccessToken2(string authority, string resource, string scope)
+        //{
+        //    string clientId = ConfigurationManager.AppSettings["AuthClientId"];
+        //    string thumbprint = ConfigurationManager.AppSettings["Thumbprint"];
+        //    var token = await GetAccessTokenFromCertificate(clientId, thumbprint, authority, resource).ConfigureAwait(false);
+
+        //    return token;
+        //}
+
+
+        //private static Dictionary<string, X509Certificate2> loadedCertificatePfx = new Dictionary<string, X509Certificate2>();
+        //private static object _lockInitCertificate = new object();
+
+        //private static X509Certificate2 FindCertificateByThumbprint(string thumbprint, StoreLocation storeLocation)
+        //{
+        //    using (var store = new X509Store(StoreName.My, storeLocation))
+        //    {
+        //        store.Open(OpenFlags.ReadOnly);
+
+        //        // Don't validate certs, since the test root isn't installed.
+        //        var col = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+
+        //        if (col == null || col.Count == 0)
+        //        {
+        //            return null;
+        //        }
+        //        else
+        //        {
+        //            return col[0];
+        //        }
+        //    }
+        //}
+
+        //private static X509Certificate2 InitializeForCertificate(string thumprint)
+        //{
+        //    X509Certificate2 certificatePfx;
+
+        //    loadedCertificatePfx.TryGetValue(thumprint, out certificatePfx);
+        //    if (certificatePfx == null)
+        //    {
+        //        lock (_lockInitCertificate)
+        //        {
+        //            loadedCertificatePfx.TryGetValue(thumprint, out certificatePfx);
+        //            if (certificatePfx == null)
+        //            {
+        //                certificatePfx = FindCertificateByThumbprint(thumprint, StoreLocation.CurrentUser);
+
+        //                //If certificate is not found in User store then try in the Machine store
+        //                if (certificatePfx == null)
+        //                {
+        //                    certificatePfx = FindCertificateByThumbprint(thumprint, StoreLocation.LocalMachine);
+        //                }
+
+        //                if (certificatePfx != null)
+        //                {
+        //                    loadedCertificatePfx.Add(thumprint, certificatePfx);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return (certificatePfx);
+        //}
+
+        //private static async Task<string> GetAccessTokenFromCertificate(string clientId, string thumprint, string authority, string resource)
+        //{
+        //    X509Certificate2 certificate = InitializeForCertificate(thumprint);
+        //    var clientAssertionCertificate = new ClientAssertionCertificate(clientId, certificate);
+        //    AuthenticationContext authenticationContext = new AuthenticationContext(authority, TokenCache.DefaultShared);
+
+        //    var result = await authenticationContext.AcquireTokenAsync(resource, clientAssertionCertificate).ConfigureAwait(false);
+        //    return result.AccessToken;
+        //}
     }
 }
---
+++ DemoAKV3/Utils/CertificateAccesTokenService.cs
@@ -0,0 +1,121 @@
+﻿using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace DemoAKV3.Utils
+{
+    /// <summary>
+    /// Obtenir un jeton d'accès via un certificat
+    /// </summary>
+    public class CertificateAccesTokenService
+    {
+        #region Constructeur
+        readonly string _clientId;
+        readonly string _thumbprint;
+
+        public CertificateAccesTokenService()
+        {
+            _clientId = ConfigurationManager.AppSettings["AuthClientId"];
+            _thumbprint = ConfigurationManager.AppSettings["Thumbprint"];
+        } 
+        #endregion
+
+        #region GetAccessToken
+        /// <summary>
+        /// Réclamer un jeton d'authentification au vault
+        /// </summary>
+        /// <param name="authority"></param>
+        /// <param name="resource"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        public async Task<string> GetAccessToken(string authority, string resource, string scope)
+        {
+            string token = await GetAccessTokenFromCertificate(_clientId, _thumbprint, authority, resource).ConfigureAwait(false);
+
+            return token;
+        }
+        #endregion
+
+        #region GetAccessTokenFromCertificate (private)
+        /// <summary>
+        /// Obtenir le jeton
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <param name="thumprint"></param>
+        /// <param name="authority"></param>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        private async Task<string> GetAccessTokenFromCertificate(string clientId, string thumprint, string authority, string resource)
+        {
+            X509Certificate2 certificate = FindCertificate(thumprint);
+            ClientAssertionCertificate clientAssertionCertificate = new ClientAssertionCertificate(clientId, certificate);
+            AuthenticationContext authenticationContext = new AuthenticationContext(
+                authority, 
+                TokenCache.DefaultShared);
+
+            AuthenticationResult result = await authenticationContext
+                .AcquireTokenAsync(resource, clientAssertionCertificate)
+                .ConfigureAwait(false);
+
+            return result.AccessToken;
+        }
+        #endregion
+
+        #region FindCertificate (private)
+        /// <summary>
+        /// Recherche un certificat dans les différents magasins à partir de son empreinte
+        /// </summary>
+        /// <param name="thumprint"></param>
+        /// <returns></returns>
+        private X509Certificate2 FindCertificate(string thumprint)
+        {
+            X509Certificate2 certificatePfx;
+            // on recherche tout d'abord dans le magasin de l'utilisateur
+            certificatePfx = FindCertificateByThumbprint(thumprint, StoreLocation.CurrentUser);
+
+            if (certificatePfx == null)
+            {
+                // pas trouvé
+                // on cherche alors dans le magasin de l'ordinateur lpcal
+                certificatePfx = FindCertificateByThumbprint(thumprint, StoreLocation.LocalMachine);
+            }
+
+            return (certificatePfx);
+        }
+        #endregion
+
+        #region FindCertificateByThumbprint (private)
+        /// <summary>
+        /// Recherche un certificat dans un magasin donné étant connue son empreinte
+        /// </summary>
+        /// <param name="thumbprint"></param>
+        /// <param name="storeLocation"></param>
+        /// <returns></returns>
+        private X509Certificate2 FindCertificateByThumbprint(string thumbprint, StoreLocation storeLocation)
+        {
+            // obtient une instance d'un proxy vers le magasin
+            using (X509Store store = new X509Store(StoreName.My, storeLocation))
+            {
+                // ouvre le magasin en lecture seule
+                store.Open(OpenFlags.ReadOnly);
+
+                // recherche les certificats qui répondent au critère
+                X509Certificate2Collection col = store
+                    .Certificates
+                    .Find(X509FindType.FindByThumbprint, thumbprint, false);
+                if (col == null || col.Count == 0)
+                {
+                    // pas de certificat candidat
+                    return null;
+                }
+                else
+                {
+                    // on retourne le premier certificat de la liste
+                    return col[0];
+                }
+            }
+        } 
+        #endregion
+    }
+}
