12d7b573150ed8baa5b135b7a41a93a6e3b36f67
De Lène Mirouze <fmirouze@hotmail.com>
2018-09-26 13:38:59 +0200

    certificats

--- DemoAKV3/Program.cs
+++ DemoAKV3/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using DemoAKV3.Utils;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.KeyVault.WebKey;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Rest.Azure;
 
@@ -19,13 +18,16 @@ namespace DemoAKV3
         {
             string baseVaultAddress = "https://amethystevault.vault.azure.net";
 
+            // Azure AD
             //KeyVaultClient keyVaultClient = new KeyVaultClient(
             //        new KeyVaultClient.AuthenticationCallback(GetAccessToken));
 
+            // Certificat
             CertificateAccesTokenService cts = new CertificateAccesTokenService();
             KeyVaultClient keyVaultClient = new KeyVaultClient(
                     new KeyVaultClient.AuthenticationCallback(cts.GetAccessToken));
 
+            #region Secret
             Console.WriteLine("Liste des secrets du vault");
             IPage<SecretItem> items = keyVaultClient
                 .GetSecretsAsync(baseVaultAddress)
@@ -33,11 +35,11 @@ namespace DemoAKV3
 
             foreach (SecretItem item in items)
             {
-                string name= item.Identifier.Name;
+                string name = item.Identifier.Name;
                 SecretBundle s = keyVaultClient
                     .GetSecretAsync(baseVaultAddress, name)
                     .Result;
-          
+
                 Console.WriteLine("{0}: {1}", name, s.Value);
             }
             Console.WriteLine();
@@ -52,7 +54,57 @@ namespace DemoAKV3
             //    .Result;
 
             Console.WriteLine(secret.Value);
+            #endregion
+
+            #region Key
+            string keyName = "keyDemo";
+            string identifiant = baseVaultAddress + "/keys/" + keyName;
+
+            // proxy 
+            KeyBundle key = keyVaultClient
+                .GetKeyAsync(baseVaultAddress, keyName)
+                .Result;
+
+            // encode un message
+            byte[] byteMessage = Encoding.Default.GetBytes("Salut le monde!");
+            KeyOperationResult result = keyVaultClient
+                .EncryptAsync(identifiant, JsonWebKeyEncryptionAlgorithm.RSAOAEP, byteMessage)
+                .Result;
+            string encoded = Convert.ToBase64String(result.Result);
+
+            Console.WriteLine("Message encodé: {0}", encoded);
+
+            // décode le message
+            byteMessage = Convert.FromBase64String(encoded);
+            result = keyVaultClient
+                .DecryptAsync(identifiant, JsonWebKeyEncryptionAlgorithm.RSAOAEP, byteMessage)
+                .Result;
+
+            string message = Encoding.Default.GetString(result.Result);
+            Console.WriteLine("Après décodage: {0}", message);
+            #endregion
+
+            #region Certificate
+            // proxy 
+            CertificateBundle cert = keyVaultClient
+                .GetCertificateAsync(baseVaultAddress, "TestCertificate")
+                .Result;
+
+            Console.WriteLine(cert.X509Thumbprint.ToHexString());
+
+            // proxy
+            SecretBundle cert2 = keyVaultClient
+                .GetSecretAsync(baseVaultAddress + "/secrets/TestCertificate")
+                .Result;
+
+            byte[] privateKeyBytes = Convert.FromBase64String(cert2.Value);
+            X509Certificate2 pfx = new X509Certificate2(privateKeyBytes, (string)null, X509KeyStorageFlags.MachineKeySet);
+
+            Console.WriteLine(pfx.Thumbprint);
+            #endregion
         }
+
+    
 
         private static async Task<string> GetAccessToken(string authority, string resource, string scope)
         {
@@ -69,77 +121,5 @@ namespace DemoAKV3
             return authResult.AccessToken;
         }
 
-        //private static async Task<string> GetAccessToken2(string authority, string resource, string scope)
-        //{
-        //    string clientId = ConfigurationManager.AppSettings["AuthClientId"];
-        //    string thumbprint = ConfigurationManager.AppSettings["Thumbprint"];
-        //    var token = await GetAccessTokenFromCertificate(clientId, thumbprint, authority, resource).ConfigureAwait(false);
-
-        //    return token;
-        //}
-
-
-        //private static Dictionary<string, X509Certificate2> loadedCertificatePfx = new Dictionary<string, X509Certificate2>();
-        //private static object _lockInitCertificate = new object();
-
-        //private static X509Certificate2 FindCertificateByThumbprint(string thumbprint, StoreLocation storeLocation)
-        //{
-        //    using (var store = new X509Store(StoreName.My, storeLocation))
-        //    {
-        //        store.Open(OpenFlags.ReadOnly);
-
-        //        // Don't validate certs, since the test root isn't installed.
-        //        var col = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
-
-        //        if (col == null || col.Count == 0)
-        //        {
-        //            return null;
-        //        }
-        //        else
-        //        {
-        //            return col[0];
-        //        }
-        //    }
-        //}
-
-        //private static X509Certificate2 InitializeForCertificate(string thumprint)
-        //{
-        //    X509Certificate2 certificatePfx;
-
-        //    loadedCertificatePfx.TryGetValue(thumprint, out certificatePfx);
-        //    if (certificatePfx == null)
-        //    {
-        //        lock (_lockInitCertificate)
-        //        {
-        //            loadedCertificatePfx.TryGetValue(thumprint, out certificatePfx);
-        //            if (certificatePfx == null)
-        //            {
-        //                certificatePfx = FindCertificateByThumbprint(thumprint, StoreLocation.CurrentUser);
-
-        //                //If certificate is not found in User store then try in the Machine store
-        //                if (certificatePfx == null)
-        //                {
-        //                    certificatePfx = FindCertificateByThumbprint(thumprint, StoreLocation.LocalMachine);
-        //                }
-
-        //                if (certificatePfx != null)
-        //                {
-        //                    loadedCertificatePfx.Add(thumprint, certificatePfx);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return (certificatePfx);
-        //}
-
-        //private static async Task<string> GetAccessTokenFromCertificate(string clientId, string thumprint, string authority, string resource)
-        //{
-        //    X509Certificate2 certificate = InitializeForCertificate(thumprint);
-        //    var clientAssertionCertificate = new ClientAssertionCertificate(clientId, certificate);
-        //    AuthenticationContext authenticationContext = new AuthenticationContext(authority, TokenCache.DefaultShared);
-
-        //    var result = await authenticationContext.AcquireTokenAsync(resource, clientAssertionCertificate).ConfigureAwait(false);
-        //    return result.AccessToken;
-        //}
     }
 }
---
+++ DemoAKV3/Utils/CertificateHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAKV3.Utils
+{
+    public static class CertificateHelper
+    {
+        public static string ToHexString(this byte[] hex)
+        {
+            if (hex == null)
+                return null;
+            if (hex.Length == 0)
+                return string.Empty;
+
+            var s = new StringBuilder();
+            foreach (byte b in hex)
+            {
+                s.Append(b.ToString("x2"));
+            }
+            return s.ToString();
+        }
+    }
+}
